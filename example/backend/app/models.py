27d99b0aa5e42719a77221a810a88c13affd7bb2
DESKTOP-BIT8QND\wnsgh <ths3630802@naver.com>
2021-10-19 14:34:35 +0900

    create

--- example/backend/app/models.py
+++ example/backend/app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Notice(models.Model) :
+    images = models.ImageField(null = True , blank = True, upload_to = "images")
+    title = models.CharField(null=True , max_length=500)
+    body = models.CharField(null = True, max_length=500)
+    userName = models.CharField(null = True, max_length=500)
