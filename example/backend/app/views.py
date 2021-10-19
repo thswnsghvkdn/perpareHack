70a9dbe000c38add2da0d2ba993107147e7e3d32
DESKTOP-BIT8QND\wnsgh <ths3630802@naver.com>
2021-10-19 14:34:35 +0900

    create
    (cherry picked from commit 27d99b0aa5e42719a77221a810a88c13affd7bb2)

--- example/backend/app/views.py
+++ example/backend/app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Notice
+from django.contrib.auth.models import User
+def home(request):
+    #if request.method == "POST":
+    return render(request , 'notice.html')
 
-# Create your views here.
+def write(request):
+    if request.method == 'POST':
+        t = request.POST['title']
+        b = request.POST['board']
+        i = request.FILE['image']
+        u= request.user.username
+        Notice.objects.create(title = c , body = b , userName = u , image = i)
+        return redirect('/')
+    else :
+        return render(request , 'write.html')
